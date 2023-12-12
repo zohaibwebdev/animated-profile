ede13034571a5bb61d8d1f125fe20d9ab1c5e785
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2023-12-12 11:47:58 +0500

    portfolio section complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 const App = () => {
@@ -20,9 +21,7 @@ const App = () => {
       <section>
         <Parallax type="portfolio" />
       </section>
-      <section id="Portfolio">Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
+      <Portfolio />
       <section id="Contact">Contact</section>
     </div>
   );
